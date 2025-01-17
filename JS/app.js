d198887264a13b933d61fa3df5ac37f93dd502b3
AliShakeri83 <ali.sh87873@gmail.com>
2025-01-17 17:27:31 +0330

    update

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,13 @@
 let sizes = document.querySelectorAll('#sizeProduct');
+let colorProduct = document.querySelectorAll('#colorProduct')
+let imgProduct = document.querySelector('.imgProduct')
+
+let allColorProduct = [
+    {id:1,src: './IMG/image (1).png'},
+    {id:2,src: './IMG/image (2).png'},
+    {id:3,src: './IMG/image.png'},
+    {id:4,src: './IMG/image (3).png'}
+]
 
 sizes.forEach((size) => {
     size.addEventListener('click', (e) => {
@@ -9,3 +18,15 @@ sizes.forEach((size) => {
         item.classList.add('focus');
     });
 });
+
+
+colorProduct.forEach((color) => {
+    color.addEventListener('click', (e) => {
+        // console.log(e.target.classList[0]);
+        let findIndex = allColorProduct.find((id) => {
+            return id.id == e.target.classList[0]
+        })
+        console.log(findIndex);
+        imgProduct.setAttribute('src', findIndex.src)
+    })
+})
